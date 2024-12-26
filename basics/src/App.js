05550cdcf64c63c819eca020a84e914ddfb39555
rabbu014 <rabbanijamadar7@gmail.com>
2024-12-26 12:50:30 +0530

    props concept

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,7 +1,19 @@
+import Contact from "./Contact";
+import About from "./About";
+import Home from "./Home";
+import Greeting from "./Greeting";
 
 function App() {
   return (
+    <div>
     <h1>Hello, from KodNest</h1>
+    <Greeting name = {'Akash'} />
+    <Greeting name = {'Anil'} />
+
+    <Home />
+    <Contact />
+    <About />
+    </div>
   );
 }
 
---
+++ basics/src/Greeting.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function Greeting(props) {
+    return (
+        <div>
+            <h3>Hello {props.name}</h3>
+        </div>
+    )
+}
